1b69ff6f808a160f63840327a1115f2edcddddfb
diogo codinha <diogoomendees99@gmail.com>
2023-07-24 18:05:57 +0100

    last changes in the game

---
+++ index.js
@@ -0,0 +1,132 @@
+let playerScore = 0
+let computerScore = 0
+let roundWinner = ''
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection === computerSelection){roundWinner = 'tie'}
+
+if((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+    (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+    (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+){
+    playerScore++
+    roundWinner = 'player'
+}else{
+    computerScore++
+    roundWinner = 'computer'
+}
+    updateScoreMessage(roundWinner, playerSelection, computerSelection)
+}
+console.log(playRound);
+function getRandomChoices(){
+    let randomNumber = Math.floor(Math.random()* 3)
+    switch(randomNumber){
+        case 0:
+            return 'ROCK';
+        case 1: 
+            return 'PAPER';
+        case 2: 
+            return 'SCISSORS'
+    }
+}
+
+function isGameOver(){
+    return playerScore === 5 || computerScore === 5
+}
+const scoreInfo = document.getElementById('scoreInfo')
+const scoreMessage = document.getElementById('scoreMessage')
+const playerScorePara = document.getElementById('playerScore')
+const computerScorePara = document.getElementById('computerScore')
+const playerSign = document.getElementById('playerSign')
+const computerSign = document.getElementById('computerSign')
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+const playAgain = document.getElementById('playAgain')
+
+function updateChoices(playerSelection, computerSelection) {
+    switch (playerSelection) {
+      case 'ROCK':
+        playerSign.textContent = '✊'
+        break
+      case 'PAPER':
+        playerSign.textContent = '✋'
+        break
+      case 'SCISSORS':
+        playerSign.textContent = '✌'
+        break
+    }
+  
+    switch (computerSelection) {
+      case 'ROCK':
+        computerSign.textContent = '✊'
+        break
+      case 'PAPER':
+        computerSign.textContent = '✋'
+        break
+      case 'SCISSORS':
+        computerSign.textContent = '✌'
+        break
+    }
+  }
+  rockBtn.addEventListener('click', () => handleClick('ROCK'))
+  paperBtn.addEventListener('click', () => handleClick('PAPER'))
+  scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'))
+  playAgain.addEventListener('click', () => restartGame())
+
+  function handleClick(playerSelection) {
+    if(!isGameOver()){
+    const computerSelection = getRandomChoices();
+    playRound(playerSelection, computerSelection);
+    updateChoices(playerSelection, computerSelection);
+    updateScore();
+    if(isGameOver()){
+      rockBtn.disabled = true;
+      paperBtn.disabled = true;
+      scissorsBtn.disabled = true;
+    if(playerScore > computerScore){alert('you win the game!!')}
+    else if (playerScore < computerScore){alert('the computer win the game!!')}
+    else{alert('its a tie!')}
+    }
+  }
+  }
+  
+  
+  function updateScore() {
+    if (roundWinner === 'tie') {
+      scoreInfo.textContent = "It's a tie!"
+    } else if (roundWinner === 'player') {
+      scoreInfo.textContent = 'You won!'
+    } else if (roundWinner === 'computer') {
+      scoreInfo.textContent = 'You lost!'
+    }
+  
+    playerScorePara.textContent = `Player: ${playerScore}`
+    computerScorePara.textContent = `Computer: ${computerScore}`
+  }
+  
+  function updateScoreMessage(winner, playerSelection, computerSelection) {
+    if (winner === 'player') {
+      scoreMessage.textContent = `${capitalizeFirstLetter(
+        playerSelection
+      )} beats ${computerSelection.toLowerCase()}`
+      return
+    }
+    if (winner === 'computer') {
+      scoreMessage.textContent = `${capitalizeFirstLetter(
+        playerSelection
+      )} is beaten by ${computerSelection.toLowerCase()}`
+      return
+    }
+  
+    scoreMessage.textContent = `${capitalizeFirstLetter(
+      playerSelection
+    )} ties with ${computerSelection.toLowerCase()}`
+  }
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+  }
+  function restartGame(){
+    window.location.reload()
+  }
+  
